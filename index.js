66aee90ae8218154a4c3cd92297b36227ef22a30
Jhon S Garcia Izquierdo <metalprime57@gmail.com>
2020-10-17 15:59:22 -0500

    update JS and firebase start

---
+++ index.js
@@ -0,0 +1,10 @@
+const btnCandidate = document.querySelector('.vote__btnCandidate');
+const btnVotation = document.querySelector('.vote__btnVotation');
+
+const inputIdCand = document.querySelector('.vote__inputIdCand');
+const inputNameCand = document.querySelector('.vote__inputNameCand');
+const btnRegister = document.querySelector('.vote__btnRegister');
+
+const inputIDvote = document.querySelector('.vote__inputIDvote');
+const btnVote = document.querySelector('.vote__btnVote');
+
