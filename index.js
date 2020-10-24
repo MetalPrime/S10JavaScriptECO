5c72332325147bcbd0b3d454a38d0446cddcb4f4
Jhon Garcia <metalprime57@gmail.com>
2020-10-23 23:59:29 -0500

    update js: creation of database and add elements

--- index.js
+++ index.js
@@ -10,53 +10,81 @@ const btnVote = document.querySelector('.vote__btnVote');
 
 
 var firebaseConfig = {
-    apiKey: "AIzaSyAJAb5xt0drUeCa2dBKMhHf3z8QMaHI2y4",
-    authDomain: "s10project.firebaseapp.com",
-    databaseURL: "https://s10project.firebaseio.com",
-    projectId: "s10project",
-    storageBucket: "s10project.appspot.com",
-    messagingSenderId: "830069995055",
-    appId: "1:830069995055:web:e554157ae77f0ee3dbf8da",
-    measurementId: "G-B557F9P5NT"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  firebase.analytics();
-  
-  // Get a reference to the database service
-  var database = firebase.database();
+  apiKey: "AIzaSyAJAb5xt0drUeCa2dBKMhHf3z8QMaHI2y4",
+  authDomain: "s10project.firebaseapp.com",
+  databaseURL: "https://s10project.firebaseio.com",
+  projectId: "s10project",
+  storageBucket: "s10project.appspot.com",
+  messagingSenderId: "830069995055",
+  appId: "1:830069995055:web:e554157ae77f0ee3dbf8da",
+  measurementId: "G-B557F9P5NT"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+// Get a reference to the database service
+var database = firebase.database();
 
 
 
-  //Registrar a los candidatos
-btnRegister.addEventListener("click",function(){
-    
+//Registrar a los candidatos
+btnRegister.addEventListener("click", function () {
+
+  objNewCandidate = {
+    ID: inputIdCand.value,
+    name: inputNameCand.value,
+  }
+
+  database.ref('candidates/' + objNewCandidate.ID).set(objNewCandidate);
+  console.log(objNewCandidate);
+
 });
 
 
 //Votar por los candidatos
-btnVotation.addEventListener("click",function(){
-    
+btnVotation.addEventListener("click", function () {
+
+  objVote = {
+    IDVote: inputIDvote.value,
+  }
+
+  database.ref('votes/').push().set(objVote);
+
 });
 
 
 
 
 //Candidatos visibles
-btnCandidate.addEventListener("click",function(){
-    alert("estoy vivo");
+btnCandidate.addEventListener("click", function () {
+  var listCandidate = [];
+
+  database.ref('candidates').on('value',
+    function (elem) {
+
+      elem.forEach(
+        function (i) {
+          listCandidate.push(i.val().name + '\n');
+        }
+      );
+    }
+
+  );
+
+  alert(listCandidate);
 });
 
 //Puntajes en porcentajes visibles
-btnVotation.addEventListener("click",function(){
-    
+btnVotation.addEventListener("click", function () {
+
 });
 
 
 
 
-database.ref('votes').transaction( candidates => {
-  candidate && (candidates['pepito'] += 1);
+/*database.ref('votes').transaction( candidates => {
+  //candidate && (candidates['pepito'] += 1);
   return candidates;
 })
 
@@ -64,3 +92,4 @@ database.ref('votes').set( {
   pepito: 0,
   juanito: 2
 })
+*/
